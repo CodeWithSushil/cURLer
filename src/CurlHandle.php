b4ef3893d110789617a90e934b845f14a0730d7e
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-04-06 20:50:30 +0530

    curl

---
+++ src/CurlHandle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CurlWrapper;
+
+use CurlWrapper\Traits\CurlHelperTrait;
+use CurlWrapper\Traits\LoggerTrait;
+
+final class CurlHandle implements CurlInterface
+{
+    use CurlHelperTrait, LoggerTrait;
+
+    // class code...
+}
